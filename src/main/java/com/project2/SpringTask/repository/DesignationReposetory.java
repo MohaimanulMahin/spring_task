317ca53a279f99a656b731039b443ad01d0d1c9c
MohaimanulMahin <mohaimanul.mahin@digigate360.com>
2022-06-21 17:48:24 +0600

    f

--- src/main/java/com/project2/SpringTask/repository/DesignationReposetory.java
+++ src/main/java/com/project2/SpringTask/repository/DesignationReposetory.java
@@ -2,7 +2,11 @@ package com.project2.SpringTask.repository;
 
 import com.project2.SpringTask.entity.Designation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
+//@Repository
+@RepositoryRestResource
 public interface DesignationReposetory extends MyBaseRepository<Designation, Long> {
 
 }
